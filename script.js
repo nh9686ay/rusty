769891efc07b2ac10bb7b8861dc7147165ad9c43
Emon <89279974+nh9686ay@users.noreply.github.com>
2022-07-08 19:26:09 -0500

    object model

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
-// document.getElementById("count").innerText = 5
+// grab the count-el element, store it in a countEl variable
+let count = 0
 
-// let count = 5
-// count = count + 1
-// console.log(count)
-
-// intialize the count as 0
-// listen for clicks on the increment button
-// increment the count variable when the button is clicked (log it out)
-// change the count-el in the HTML to reflect the new count
-
-let countEl = document.getElementById("count-el")
-// console.log(countEl)
-
-let count = 0;
 function increment() {
     count = count + 1
-    countEl.innerText = count;
-    // console.log(count)
+    // set countEl's innerText to the count
 }
+
+
+
+//DOM - document object model
