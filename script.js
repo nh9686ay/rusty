c798366eb00fe2d224d71ba7a497d23dfaf111e7
Emon <89279974+nh9686ay@users.noreply.github.com>
2022-07-09 09:31:15 -0500

    app completed/train station count

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-// 1. Grab the save-el paragrah and store it in a variable called saveEl
-let saveEl = document.getElementById("save-el");
+let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
 
 function increment() {
     count += 1
-    countEl.innerText = count
+    countEl.textContent = count
 }
 
 function save() {
-    entries = count + " - "
-    saveEl.textContent += entries;
+    let countStr = count + " - "
+    saveEl.textContent += countStr;
+    countEl.textContent = 0;
+    count = 0;
 }
-
