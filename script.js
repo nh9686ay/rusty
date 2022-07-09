8c86e1a56a27f98908e2110afe14e201b98f4030
Emon <89279974+nh9686ay@users.noreply.github.com>
2022-07-09 08:34:12 -0500

    function save()

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-// grab the count-el element, store it in a countEl variable
+let countEl = document.getElementById("count-el")
 let count = 0
 
 function increment() {
     count = count + 1
-    // set countEl's innerText to the count
+    countEl.innerText = count
 }
 
+// 1. Create a function, save(), which logs out the count when it's called
 
 
-//DOM - document object model
+function save(){
+    console.log(count)
+}
+
+save()
